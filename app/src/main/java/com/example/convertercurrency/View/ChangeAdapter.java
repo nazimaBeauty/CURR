4280259adfe5a7c885c41d80f57932698076854a
nurskanyb <nurskanyb@gmail.com>
2023-01-03 17:17:41 +0300

    My first commit to gitlab and source tree

--- app/src/main/java/com/example/convertercurrency/View/ChangeAdapter.java
+++ app/src/main/java/com/example/convertercurrency/View/ChangeAdapter.java
@@ -30,9 +30,7 @@ public class ChangeAdapter extends RecyclerView.Adapter<ChangeAdapter.ViewHolder
 
     @NonNull
     @Override
-    public ViewHolder
-    onCreateViewHolder(@NonNull ViewGroup parent,
-                       int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_change, parent, false);
         return new ViewHolder(view);
     }
